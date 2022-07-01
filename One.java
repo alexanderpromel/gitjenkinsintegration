9f5c1fff9b59b1778b514764ecf506d903d1d686
alexanderpromel <alexanderpromel@gmail.com>
2022-07-01 22:53:27 +0530

    added my fiLE

---
+++ One.java
@@ -0,0 +1,12 @@
+package org.digits;
+
+public class One{
+	public void primeNumbers() {
+		System.out.println("Rise and shine");
+	}
+	public static void main(String[]args) {
+		One a = new One();
+		a.primeNumbers();
+	}
+	
+}
